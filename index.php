7e9c69e6f09dd6ef053a4ba0e4bf233518e43dd3
Andrea Miglietta <m.andrea96@hotmail.it>
2022-10-13 19:59:15 +0200

    aggiunta commenti

--- index.php
+++ index.php
@@ -7,20 +7,28 @@
 
 <?php
 
+    // paragraph
     $paragraph = "Growing up in the Netherlands, with its network of pathways, its flat landscape and its bicycle-friendly traffic laws, the brothers Ties and Taco Carlier were commuting with their parents on bikes by age 4.";
 
+    // paragraph length
     $paragraphLength = strlen($paragraph);
 
+    // word to be censored
     $badWord = $_GET ['badWord'];
 
+    // censored paragraph
     $censoredParagraph = str_replace($badWord, '***', $paragraph);
 ?>
 
+
+<!-- not censored paragraph -->
 <div>
+    <!-- paragraph -->
     <p>
         PARAGRAPH: <?= $paragraph ?>
     </p>
 
+    <!-- paragraph length -->
     <p>
         PARAGRAPH LENGTH: <?= $paragraphLength ?>
     </p>

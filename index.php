5c0e32cc1ffb75084cc51c4b8ffb5a580dd690f5
Andrea Miglietta <m.andrea96@hotmail.it>
2022-10-13 19:51:10 +0200

    creazione paragrafo

--- index.php
+++ index.php
@@ -4,3 +4,9 @@
     Una parola da censurare viene passata dall'utente tramite parametro GET.
     Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. 
 -->
+
+<?php
+
+    $paragraph = "Growing up in the Netherlands, with its network of pathways, its flat landscape and its bicycle-friendly traffic laws, the brothers Ties and Taco Carlier were commuting with their parents on bikes by age 4.";
+
+?>
